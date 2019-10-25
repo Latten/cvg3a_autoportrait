4a1c17eb35d68dd974b754b6df450d34ccf63e08
Latten <lucas.r.martiny@gmail.com>
2019-10-25 15:32:59 +0200

    bugfixes

--- assets/scripts/preface.js
+++ assets/scripts/preface.js
@@ -5,15 +5,15 @@ function prefaceAnimation() {
 	// $('#prefaceCharacter').classList.add('fall') ;
 	document.getElementById('fallingMan').classList.add('falling');
 	wait(500);
-	$('#preface').hide(200);
-	$('#preface-container').fadeTo(400, 0)
-	$('#preface-container').hide(200);
+	$('#preface').hide(2000);
+	$('#preface-container').hide(2000);
 	$('#app-container').show();
 	wait(500); 
 	followPlayerButton() ; 
 	setBackgroundColor(0); 
 	document.getElementById('app-container').scrollLeft = 0 ; 
-	$('#game').fadeTo(0, 0.5); wait(700); 
+	$('#game').fadeTo(0, 0.0); wait(700); 
 	gameContainerResize(); 
-	setRatioFromPhaser(); SMPplay('mplayer01');
+	setRatioFromPhaser(); 
+	SMPplay('mplayer01');
 }
